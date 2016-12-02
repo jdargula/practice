1698a2889bfd7db28288657cc2f5f7b3eb0c002c
Jayaram Dargula <jdargula3@gatech.edu>
2016-12-01 21:12:49 -0500

    update C

--- C.java
+++ C.java
@@ -1,6 +1,5 @@
-public class C extends RuntimeException {
-    public C(String arg) {
-        super(arg);
-        System.out.print("boom!");
+class C {
+    public static void main(String[] args) {
+        System.out.print("kek");
     }
 }
